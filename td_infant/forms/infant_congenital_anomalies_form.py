36f200da70d8c71db8d6b8a03e8478d38d6534ae
Deji <soluwamayowa@bhp.org.bw>
2018-12-07 12:07:40 +0200

    updating congenital anomalies forms

--- td_infant/forms/infant_congenital_anomalies_form.py
+++ td_infant/forms/infant_congenital_anomalies_form.py
@@ -1,57 +1,90 @@
 from django import forms
+from ..models import (InfantCongenitalAnomalies, InfantCardioDisorder,
+                      InfantMusculoskeletal, InfantRenal, InfantFemaleGenital,
+                      InfantCleftDisorder, InfantFacialDefect, InfantMouthUpGi,
+                      InfantRespiratoryDefect, InfantLowerGi, InfantMaleGenital,
+                      InfantSkin, InfantCns, InfantTrisomies)
 
 
 class InfantCongenitalAnomaliesForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantCongenitalAnomalies
+        fields = '__all__'
 
 
 class InfantFacialDefectForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantFacialDefect
+        fields = '__all__'
 
 
 class InfantCleftDisorderForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantCleftDisorder
+        fields = '__all__'
 
 
 class InfantMouthUpGiForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantMouthUpGi
+        fields = '__all__'
 
 
 class InfantCardioDisorderForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantCardioDisorder
+        fields = '__all__'
 
 
 class InfantRespiratoryDefectForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantRespiratoryDefect
+        fields = '__all__'
 
 
 class InfantLowerGiForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantLowerGi
+        fields = '__all__'
 
 
 class InfantFemaleGenitalForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantFemaleGenital
+        fields = '__all__'
 
 
 class InfantMaleGenitalForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantMaleGenital
+        fields = '__all__'
 
 
 class InfantRenalForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantRenal
+        fields = '__all__'
 
 
 class InfantMusculoskeletalForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantMusculoskeletal
+        fields = '__all__'
 
 
 class InfantSkinForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantSkin
+        fields = '__all__'
 
 
 class InfantTrisomiesForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantTrisomies
+        fields = '__all__'
 
 
 class InfantCnsForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = InfantCns
+        fields = '__all__'
