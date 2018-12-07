f56777e58bdaea88bf47f7e79b6d6cc82cd3702e
Deji <soluwamayowa@bhp.org.bw>
2018-12-07 11:48:39 +0200

    updating solid food assessment form

--- td_infant/forms/solid_food_assessment_form.py
+++ td_infant/forms/solid_food_assessment_form.py
@@ -1,6 +1,9 @@
 from django import forms
+from ..models import SolidFoodAssessment
 
 
 class SolidFoodAssessmentForm(forms.ModelForm):
 
-    pass
+    class Meta:
+        model = SolidFoodAssessment
+        fields = '__all__'
