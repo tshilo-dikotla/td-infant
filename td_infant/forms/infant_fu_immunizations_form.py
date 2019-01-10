12d46e55ee58e71ba3d980fb4ef2987a3c95c811
Ame Diphoko <diphokoame@gmail.com>
2019-01-10 17:08:59 +0200

    adding infant fu immunizations form validation

--- td_infant/forms/infant_fu_immunizations_form.py
+++ td_infant/forms/infant_fu_immunizations_form.py
@@ -1,8 +1,40 @@
+from django.forms import forms
 from .infant_form_mixin import InfantModelFormMixin
+from edc_constants.constants import YES
 from ..models import InfantFuImmunizations, VaccinesReceived, VaccinesMissed
 
 
 class InfantFuImmunizationsForm(InfantModelFormMixin):
+
+    def clean(self):
+        cleaned_data = super().clean()
+        vaccines_received = self.data.get(
+            'vaccinesreceived_set-0-received_vaccine_name')
+        if self.data.get('vaccines_received') == YES:
+            if not vaccines_received:
+                msg = {'vaccines_received':
+                       'You mentioned that vaccines were received. Please '
+                       'indicate which ones on the Received Vaccines table.'}
+                raise forms.ValidationError(msg)
+        else:
+            if vaccines_received:
+                raise forms.ValidationError('No vaccines received. Do not fill'
+                                            ' Received Vaccines table')
+
+        missed_vaccine_name = self.data.get(
+            'vaccinesmissed_set-0-missed_vaccine_name')
+        if self.data.get('vaccines_missed') == YES:
+            if not missed_vaccine_name:
+                msg = {'vaccines_missed':
+                       'You mentioned that the child missed some '
+                       'vaccines. Please indicate which ones in '
+                       'the Missed Vaccines table.'}
+                raise forms.ValidationError(msg)
+        else:
+            if missed_vaccine_name:
+                raise forms.ValidationError(
+                    'No vaccines missed. Do not fill Missed Vaccines table')
+        return cleaned_data
 
     class Meta:
         model = InfantFuImmunizations
