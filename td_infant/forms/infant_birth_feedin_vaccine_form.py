c6ebcfd7bb8fd927482b277a58432ce07e472b36
tumiemosweu <imosweuk@gmail.com>
2019-04-16 14:44:32 +0200

    add validation to infant feeding vaccine

--- td_infant/forms/infant_birth_feedin_vaccine_form.py
+++ td_infant/forms/infant_birth_feedin_vaccine_form.py
@@ -1,8 +1,12 @@
-from .infant_form_mixin import InfantModelFormMixin
+from td_infant_validators.form_validators import InfantBirthDataFormValidator
+
 from ..models import InfantBirthFeedingVaccine
+from .infant_form_mixin import InfantModelFormMixin
 
 
 class InfantBirthFeedinVaccineForm(InfantModelFormMixin):
+
+    orm_validator_cls = InfantBirthDataFormValidator
 
     class Meta:
         model = InfantBirthFeedingVaccine
