4302b3a695dd60e69e41b5e2601bd36a3ca80be8
Titus Diphale <tdiphale@bhp.org.bw>
2019-04-16 10:51:28 +0200

    bug fix

--- td_infant/forms/infant_birth_data_form.py
+++ td_infant/forms/infant_birth_data_form.py
@@ -1,12 +1,12 @@
+from td_infant_validators.form_validators import InfantBirthDataFormValidator
+
 from ..models import InfantBirthData
 from .infant_form_mixin import InfantModelFormMixin
-
-from td_infant_validators.form_validators import InfantBirthDataFormValidator
 
 
 class InfantBirthDataForm(InfantModelFormMixin):
 
-    orm_validator_cls = InfantBirthDataFormValidator
+    form_validator_cls = InfantBirthDataFormValidator
 
     class Meta:
         model = InfantBirthData
