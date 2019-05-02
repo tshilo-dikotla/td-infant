0d3283cd0158362124b11cf0abc58480731b65cf
tumiemosweu <imosweuk@gmail.com>
2019-05-02 16:21:38 +0200

    fix fu dx validator

--- td_infant/forms/infant_fu_dx_form.py
+++ td_infant/forms/infant_fu_dx_form.py
@@ -1,10 +1,13 @@
-from .infant_form_mixin import InfantModelFormMixin
-from ..models import InfantFuDx, InfantFuDxItems
-
+from td_infant_validators.form_validators import InfantFuDxFormValidator
 from td_infant_validators.form_validators import InfantFuDxItemsFormValidator
+
+from ..models import InfantFuDx, InfantFuDxItems
+from .infant_form_mixin import InfantModelFormMixin
 
 
 class InfantFuDxForm(InfantModelFormMixin):
+
+    form_validator_cls = InfantFuDxFormValidator
 
     class Meta:
         model = InfantFuDx
