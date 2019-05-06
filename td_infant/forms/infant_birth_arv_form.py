da924fecb7b870430766e4cd6ab3913a78d2b025
tumiemosweu <imosweuk@gmail.com>
2019-05-06 11:27:30 +0200

    fix infant requisition

--- td_infant/forms/infant_birth_arv_form.py
+++ td_infant/forms/infant_birth_arv_form.py
@@ -1,8 +1,12 @@
+from td_infant_validators.form_validators import InfantBirthArvFormValidator
+
 from ..models import InfantBirthArv
 from .infant_form_mixin import InfantModelFormMixin
 
 
 class InfantBirthArvForm(InfantModelFormMixin):
+
+    form_validator_cls = InfantBirthArvFormValidator
 
     class Meta:
         model = InfantBirthArv
