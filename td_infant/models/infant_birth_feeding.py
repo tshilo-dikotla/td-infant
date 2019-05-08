94895a1a6f1e8ff6641747a6fa252838b4cd8631
moffatmore <mofenyimoffat@gmail.com>
2019-05-08 12:19:52 +0200

    fixing infant unique together

--- td_infant/models/infant_birth_feeding.py
+++ td_infant/models/infant_birth_feeding.py
@@ -43,4 +43,4 @@ class InfantVaccines(BaseUuidModel):
     class Meta:
         verbose_name = "Infant Vaccines"
         verbose_name_plural = "Infant Vaccines"
-        unique_together = (infant_birth_feed_vaccine, vaccination)
+        unique_together = ('infant_birth_feed_vaccine', 'vaccination')
