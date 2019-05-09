845f13d46516a7ff0cd52bf32437097feff4c1fd
Leslie <leslie.kep@gmail.com>
2019-05-09 12:21:13 +0200

    uncomment code

--- td_infant/forms/infant_feeding_form.py
+++ td_infant/forms/infant_feeding_form.py
@@ -1,11 +1,12 @@
+from td_infant_validators.form_validators import InfantFeedingFormValidator
+
 from ..models import InfantFeeding
 from .infant_form_mixin import InfantModelFormMixin
 
 
-# from td_infant_validators.form_validators import InfantFeedingFormValidator
 class InfantFeedingForm(InfantModelFormMixin):
 
-    #     form_validator_cls = InfantFeedingFormValidator
+    form_validator_cls = InfantFeedingFormValidator
 
     class Meta:
         model = InfantFeeding
