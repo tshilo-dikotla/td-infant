ec7607171f2a08503b11ef664843c3a2fa911af2
moffatmore <mofenyimoffat@gmail.com>
2019-05-10 12:00:15 +0200

    fixing karabo tb form

--- td_infant/forms/karabo_tb_history_form.py
+++ td_infant/forms/karabo_tb_history_form.py
@@ -1,15 +1,14 @@
-from django import forms
-from td_maternal_validators.form_validators import (
-    MaternalTuberculosisHistoryFormValidator
+from td_infant_validators.form_validators import (
+    KaraboTBHistoryFormValidator
 )
 
 from ..models import KaraboTuberculosisHistory
 from .form_mixins import SubjectModelFormMixin
 
 
-class KaraboTuberculosisHistoryForm(SubjectModelFormMixin, forms.ModelForm):
+class KaraboTuberculosisHistoryForm(SubjectModelFormMixin):
 
-    form_validator_cls = MaternalTuberculosisHistoryFormValidator
+    form_validator_cls = KaraboTBHistoryFormValidator
 
     class Meta:
         model = KaraboTuberculosisHistory
