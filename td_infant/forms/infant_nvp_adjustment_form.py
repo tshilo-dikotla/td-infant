d783aad03ea07bc39c7fa385928f9d32ec9362c5
moffatmore <mofenyimoffat@gmail.com>
2019-05-14 15:31:53 +0200

    adding form validators in infant nvp adjustment form validator'

--- td_infant/forms/infant_nvp_adjustment_form.py
+++ td_infant/forms/infant_nvp_adjustment_form.py
@@ -1,8 +1,12 @@
 from ..models import InfantNvpAdjustment
 from .infant_form_mixin import InfantModelFormMixin
+from td_infant_validators.form_validators import (
+    InfantNvpAdjustmentFormValidator)
 
 
 class InfantNvpAdjustmentForm(InfantModelFormMixin):
+
+    form_validator_cls = InfantNvpAdjustmentFormValidator
 
     class Meta:
         model = InfantNvpAdjustment
