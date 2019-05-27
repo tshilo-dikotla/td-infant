f81eabb826b2652fd3943ec3b67bbe94f8de6fb5
MoffatMore <mofenyimoffat@gmail.com>
2019-05-27 11:24:13 +0200

    adding infant fu dx validatioons

--- td_infant/forms/infant_fu_dx_form.py
+++ td_infant/forms/infant_fu_dx_form.py
@@ -1,6 +1,8 @@
 from td_infant_validators.form_validators import InfantFuDxFormValidator
 from td_infant_validators.form_validators import InfantFuDxItemsFormValidator
 
+from django.forms import ValidationError
+
 from ..models import InfantFuDx, InfantFuDxItems
 from .infant_form_mixin import InfantModelFormMixin
 
@@ -8,6 +10,12 @@ from .infant_form_mixin import InfantModelFormMixin
 class InfantFuDxForm(InfantModelFormMixin):
 
     form_validator_cls = InfantFuDxFormValidator
+
+    def clean(self):
+        super().clean()
+        infant_fu_dx = self.data.get('infantfudxitems_set-0-fu_dx')
+        if not infant_fu_dx:
+            raise ValidationError('Please fill the table below')
 
     class Meta:
         model = InfantFuDx
