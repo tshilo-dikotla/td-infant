d74e561dd6ff279ab24d86ebf237b11d1547130d
tumiemosweu <imosweuk@gmail.com>
2019-06-18 15:39:06 +0200

    fix infant requisition

--- td_infant/forms/infant_requisition_form.py
+++ td_infant/forms/infant_requisition_form.py
@@ -3,6 +3,8 @@ from django import forms
 from django.conf import settings
 from django.utils import timezone
 from edc_base.utils import convert_php_dateformat
+from edc_form_validators import FormValidatorMixin
+
 from edc_lab.forms.modelform_mixins import RequisitionFormMixin
 from td_infant_validators.form_validators import InfantRequisitionFormValidator
 
@@ -10,7 +12,8 @@ from ..models import InfantRequisition
 from .infant_form_mixin import InfantModelFormMixin
 
 
-class InfantRequisitionForm(InfantModelFormMixin, RequisitionFormMixin):
+class InfantRequisitionForm(InfantModelFormMixin, RequisitionFormMixin,
+                            FormValidatorMixin):
 
     form_validator_cls = InfantRequisitionFormValidator
 
